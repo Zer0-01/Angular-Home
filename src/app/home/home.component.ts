4b3d4c8566960563b2f2508397b993cf03398c4d
Zer0-01 <anas.zulkifli2001@gmail.com>
2024-07-18 11:48:25 +0800

    feat: add home component

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  template: `
+   <section>
+    <form action="">
+      <input type="text" placeholder="Filter by city">
+      <button class="primary" type="button" >Search</button>
+    </form>
+  </section>
+  `,
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+}
