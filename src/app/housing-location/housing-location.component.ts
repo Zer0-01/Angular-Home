fb0fd0ab4b0bab4584826406dcb60f87451ae3f2
Zer0-01 <anas.zulkifli2001@gmail.com>
2024-07-19 11:00:54 +0800

    feat: customize the details page

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,20 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { HousingLocation } from '../housing-location';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   template: `
   <section class="listing">
   <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{ housingLocation.name }}"
     crossorigin>
-
-
-
-    
     <h2 class="listing-heading"> {{ housingLocation.name }}</h2>
     <p class="listing-location" > {{ housingLocation.city }}, {{ housingLocation.state }}</p>
+    <a [routerLink]="['/details', housingLocation.id]"></a>
   </section>
    
   `,
